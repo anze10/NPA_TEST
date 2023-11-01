45420ac2f1801323bb56ea8ef512c5276f482a00
anze10 <96385387+anze10@users.noreply.github.com>
2023-11-01 23:00:41 +0100

    vaja pet

---
+++ vaja_pet.cs
@@ -0,0 +1,11 @@
+using System;
+class vaja_pet{
+	public static void Main(string[] args){
+		for (int i = 0;i<4;i++ ){
+			for(int j = 0;j<3;j++){
+				Console.Write("*");
+			}
+			Console.WriteLine();
+		}
+	}
+}
